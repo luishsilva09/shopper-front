d08be24f5656ebc880b728ee308bd4cd0b5bd83c
Luís Henrique <luishsilva09@gmail.com>
2023-09-07 19:20:52 -0300

    feat: form de envio do arquivo csv

---
+++ src/components/Form.tsx
@@ -0,0 +1,23 @@
+import api from "../services/api";
+
+export default function FormRender(props: any) {
+  const handleOnChange = (e: any) => {
+    props.setFile(e.target.files[0]);
+  };
+
+  const handleOnSubmit = (e: React.MouseEvent) => {
+    e.preventDefault();
+    if (props.file) {
+      const data = new FormData();
+      data.append("file", props.file);
+      api.post("/sendFile", data).then((e) => props.setData(e.data));
+    }
+  };
+  return (
+    <form>
+      <h2>Selecione o arquivo</h2>
+      <input type="file" accept=".csv" onChange={handleOnChange} />
+      <button onClick={(e) => handleOnSubmit(e)}>Validar</button>
+    </form>
+  );
+}
